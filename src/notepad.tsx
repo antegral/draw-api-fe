cbec2ace6fdf60e8f252e7c9557225f9ff3df274
antegral <antegral@antegral.net>
2024-05-17 21:35:24 +0900

    Add: dummy ID generator

--- src/notepad.tsx
+++ src/notepad.tsx
@@ -36,7 +36,7 @@ const Notepad = () => {
       description: "노트가 저장되어 링크가 생성됩니다.",
     });
 
-    setLink("https://notepad.antegral.net/dLi39P2v");
+    setLink(`${window.location.origin}/${makeDummyId(6)}`);
   };
 
   return (
@@ -85,5 +85,18 @@ const Notepad = () => {
     </div>
   );
 };
+
+function makeDummyId(length: number) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
 
 export default Notepad;
